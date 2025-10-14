c6cb218a04dce7b2b9227b9d790accb4b27c933b
Laden8210 <domingo.laden@gmail.com>
2025-10-14 09:02:14 +0800

    order function partial implementation

--- app/src/main/java/com/example/dropshipping/api/ApiAddress.java
+++ app/src/main/java/com/example/dropshipping/api/ApiAddress.java
@@ -7,3 +7,5 @@ public class ApiAddress {
     public static final String imageUrl =  baseUrl +"public/images/products/";
     public static final String storeUrl =  baseUrl +"public/images/store/";
 }
+
+
